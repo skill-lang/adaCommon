c69fb0eae9d36f55b17edcd5e4fdcbc6b7c8d40d
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2015-07-29 13:47:27 +0200

    added root package to compensate missing ada syntax

---
+++ src/root.ads
@@ -0,0 +1,10 @@
+--  ___ _  ___ _ _                                                            --
+-- / __| |/ (_) | |       Common SKilL implementation                         --
+-- \__ \ ' <| | | |__     root of the package hiearachy                       --
+-- |___/_|\_\_|_|____|    by: Timm Felden                                     --
+--                                                                            --
+
+package Root is
+   pragma Pure;
+
+end Root;
